203bd2e081b248374777e53512c4186a8646a9f7
Tom Verbeure <hombre+github@gmail.com>
2018-09-25 20:09:29 -0700

    Switch Vec3D to SFix type.

--- src/main/scala/rt/CamSweep.scala
+++ src/main/scala/rt/CamSweep.scala
@@ -10,10 +10,10 @@ class CamSweep extends Component {
 
         val valid       = out(Bool)
         val ready       = in(Bool)
-        val direction   = out(Vec3D(24))
+        val direction   = out(Vec3D(3 exp, -14 exp))
     }
 
-    val dir = Reg(Vec3D(24)) init
+    val dir = Reg(Vec3D(3 exp, -14 exp))
 
 
     io.valid        := True 
@@ -21,40 +21,40 @@ class CamSweep extends Component {
 
     val width  = 400
     val height = 400
-    val stepX = (1.0/width.toFloat)*65536*(height.toFloat/width.toFloat)
-    val stepY = (1.0/height.toFloat)*65536
+//    val stepX = 1.0/width
+//    val stepY = 1.0/height
 
-    val xLeft   = (-0.5*65536).toInt
-    val yTop    = ((-0.5-0.4)*65536).toInt
-    val z       = (1.0*65536).toInt
+    val xLeft   = -0.5
+    val yTop    = 0.5-0.4
+    val z       = 1.0
     
-    val incrX = UInt(24 bits)
-    val incrY = UInt(24 bits)
+    val incrX = SFix(3 exp, -14 exp)
+    val incrY = SFix(3 exp, -14 exp)
 
-    incrX := U(stepX.toInt)
-    incrY := U(stepY.toInt)
+    incrX := 1.0/width
+    incrY := 1.0/height
 
-    val h_cntr = UInt(12 bits)
-    val v_cntr = UInt(11 bits)
+    val h_cntr = Reg(UInt(12 bits))
+    val v_cntr = Reg(UInt(11 bits))
 
     when(io.sof){
-        dir.x   := S(xLeft)
-        dir.y   := S(yTop)
-        dir.z   := S(z)
+        dir.x   := xLeft
+        dir.y   := yTop
+        dir.z   := z
 
         h_cntr  := 0
         v_cntr  := 0
     }
     .elsewhen(io.ready){
         when(h_cntr === U(width)){
-            dir.x   := S(xLeft)
-            dir.y   := dir.y + S(incrY)
+            dir.x   := xLeft
+            dir.y   := dir.y + incrY
 
             h_cntr  := 0
             v_cntr  := v_cntr + 1
         }
         .otherwise{
-            dir.x   := dir.x + S(incrX)
+            dir.x   := dir.x + incrX
         }
     }
 }
--- src/main/scala/rt/Vec3D.scala
+++ src/main/scala/rt/Vec3D.scala
@@ -3,22 +3,15 @@ package rt
 
 import spinal.core._
 
-case class Vec3DConfig(width: Int){
+case class Vec3DConfig(peak: ExpNumber, resolution: ExpNumber){
 }
 
 object Vec3D{
-    def apply(width: Int) : Vec3D = Vec3D(Vec3DConfig(width))
+    def apply(peak: ExpNumber, resolution: ExpNumber) : Vec3D = Vec3D(Vec3DConfig(peak, resolution))
 }
 
 case class Vec3D(c: Vec3DConfig) extends Bundle {
-    val x   = Scalar(c.width)
-    val y   = Scalar(c.width)
-    val z   = Scalar(c.width)
-
-    def init() : Vec3D = {
-        x   init()
-        y   init()
-        z   init()
-        this
-    }
+    val x   = SFix(c.peak, c.resolution)
+    val y   = SFix(c.peak, c.resolution)
+    val z   = SFix(c.peak, c.resolution)
 }
