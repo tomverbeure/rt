3eb190ac4d4bacdf9068fbb50b4a2d885c88e95c
Tom Verbeure <hombre+github@gmail.com>
2018-11-24 10:00:38 -0800

    fpxx math functions.

--- sw/progmem.c
+++ sw/progmem.c
@@ -18,6 +18,11 @@ static inline uint32_t rdcycle(void) {
     return cycle;
 }
 
+static inline int nop(void) {
+    asm volatile ("addi x0, x0, 0");
+    return 0;
+}
+
 void wait(int cycles)
 {
 #if 1
@@ -63,16 +68,56 @@ fpxx_t fpxx_add(fpxx_t op_a, fpxx_t op_b)
 {
     REG_WR(FPXX_OP_A, op_a);
     REG_WR(FPXX_OP_B, op_b);
-
-    return(REG_RD(FPXX_OP_ADD));
+    nop();
+    nop();
+    nop();
+    return(REG_RD(FPXX_ADD));
 }
 
 fpxx_t fpxx_mul(fpxx_t op_a, fpxx_t op_b)
 {
     REG_WR(FPXX_OP_A, op_a);
     REG_WR(FPXX_OP_B, op_b);
+    nop();
+    nop();
+    nop();
+    return(REG_RD(FPXX_MUL));
+}
 
-    return(REG_RD(FPXX_OP_MUL));
+fpxx_t int2fpxx(int op)
+{
+    REG_WR(FPXX_OP_A, op);
+    nop();
+    nop();
+    nop();
+    return(REG_RD(INT2FPXX));
+}
+
+fpxx_t fpxx_neg(fpxx_t op)
+{
+    return op ^ (1<<19);
+}
+
+fpxx_t fpxx_sub(fpxx_t op_a, fpxx_t op_b)
+{
+    return fpxx_add(op_a, fpxx_neg(op_b));
+}
+
+fpxx_t fpxx_shr(fpxx_t op_a, int shift)
+{
+    int exp = (op_a>>13)&((1<<6)-1);
+
+    if (exp == 0)
+        return op_a;
+
+    exp -= shift;
+
+    if (exp<=0)
+        return 0;
+
+    fpxx_t result = (op_a & ((1<<19) | 0x1fff)) | (exp<<13);
+
+    return result;
 }
 
 #include "fpxx_sin_table.h"
@@ -122,6 +167,7 @@ int main() {
 
     int cam_cntr = 0;
     int frame_cntr = 0;
+    int sphere_time = 64;
 
     int angle = 1;
 
@@ -133,11 +179,45 @@ int main() {
         // Blink every 60 frames.
         REG_WR(LED_CONFIG, (frame_cntr & 64)==0 && 0x7);
 
-        //float z_pos;
-        //z_pos = cam_cntr/64.0 * 5 -10;
-        //REG_WR_FP32(CAMERA_POS_Z, z_pos);
 
-        angle = (angle+1) & 0x3ff;
+        /*
+        float z_pos;
+        z_pos = cam_cntr/64.0 * 5 -10;
+        REG_WR_FP32(CAMERA_POS_Z, z_pos);
+        */
+
+        /*
+        fpxx_t z_pos;
+        z_pos = fpxx_add(fpxx_mul(float_to_fpxx(cam_cntr/64.0f), float_to_fpxx(5.0f)), float_to_fpxx(10.0f));
+        REG_WR(CAMERA_POS_Z, z_pos);
+        */
+
+        {
+            //============================================================
+            // Bounce ball
+            //============================================================
+            int min_height = 3;
+            int max_height = 10;
+            int cycle_time = 128;
+
+            fpxx_t a = fpxx_neg(fpxx_shr(fpxx_sub(int2fpxx(max_height),int2fpxx(min_height)),12));
+
+            fpxx_t s_time     = int2fpxx(sphere_time);
+            fpxx_t s_time_m_c = int2fpxx(sphere_time-cycle_time);
+            fpxx_t s_time_q   = fpxx_mul(s_time, s_time_m_c);
+            fpxx_t a_term     = fpxx_mul(s_time_q, a);
+            fpxx_t pos_y      = fpxx_add(a_term, int2fpxx(min_height));
+
+            REG_WR(SPHERE_POS_Y, pos_y);
+
+            ++sphere_time;
+            if (sphere_time == cycle_time){
+                sphere_time = 0;
+            }
+
+        }
+
+        //angle = (angle+1) & 0x3ff;
         REG_WR(ROT_Y_SIN, fpxx_sin(angle));
         REG_WR(ROT_Y_COS, fpxx_cos(angle));
 
