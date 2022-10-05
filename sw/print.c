5223f27ac33554376413cd0884ef82bfa13cbc6d
Tom Verbeure <hombre+github.com@gmail.com>
2022-10-04 21:49:45 -0700

    Using const int to optimize away multiplication.

--- sw/print.c
+++ sw/print.c
@@ -8,11 +8,11 @@ int cur_x = 0;
 int cur_y = 0;
 
 #define TXT_BUF ((volatile uint32_t *)(0x00080000 | TXT_BUF_ADDR))
-int txt_buf_width         = 80;
-int txt_buf_height        = 25;
+const int txt_buf_width         = 80;
+const int txt_buf_height        = 25;
 
-int txt_buf_active_width  = 40;
-int txt_buf_active_height = 10;
+const int txt_buf_active_width  = 40;
+const int txt_buf_active_height = 10;
 
 void clear()
 {
